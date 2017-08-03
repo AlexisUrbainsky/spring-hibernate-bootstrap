63054636603dc670f2eb2d82bd894b10e2980220
aurbainsky <aurbainsky@gmail.com>
2017-08-03 02:13:38 -0300

    Au - Adding Generic Dao for Hibernate and DBA Structure

---
+++ src/main/java/com/alexis/springhibernate/dao/AbstractDao.java
@@ -0,0 +1,42 @@
+package com.alexis.springhibernate.dao;
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class AbstractDao <PK extends Serializable,T > {
+	
+	private final Class<T> persistanceClass;
+	
+    @SuppressWarnings("unchecked")
+	public AbstractDao (){
+		this.persistanceClass = (Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[1];
+	}
+	
+    @Autowired
+	private SessionFactory sessionFactory;
+
+	protected Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+
+	@SuppressWarnings("unchecked")
+	public T getById(PK id) {
+		return (T) getSession().get(persistanceClass,id);
+	}
+	
+    public void persist(T t) {
+    	getSession().persist(t);
+    }
+	
+    public void update (T t){
+    	getSession().update(t);
+    }
+    
+    public void delete (T t) {
+    	getSession().delete(t);
+    }
+	
+}
