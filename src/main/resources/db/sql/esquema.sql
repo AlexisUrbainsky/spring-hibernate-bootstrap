87cdce718fbaa12ca2bcb90f6f2efacb9e15edf9
aurbainsky <aurbainsky@gmail.com>
2017-08-13 02:17:32 -0300

    AU - Some Changes in the Schema

--- src/main/resources/db/sql/esquema.sql
+++ src/main/resources/db/sql/esquema.sql
@@ -13,7 +13,7 @@ CREATE TABLE `person` (
   `email` varchar(45) DEFAULT NULL,
   `sex` varchar(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
   -- ONE TO MANY UN PASAPORTE TIENE MUCHOS VIAJES
 
@@ -26,34 +26,35 @@ CREATE TABLE `passport` (
   `expirationDate` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `id` FOREIGN KEY (`id`) REFERENCES `person` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
    	
 -- ideas puedo pedir segun el pais, que pidan como EEUU la visa para viajes,
 -- restricciones de seguridad para criminales, libre ingreso y salida del pais para otros
   
-CREATE TABLE `springhibernate`.`countries` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `country_code` VARCHAR(2) NOT NULL,
-  `country_name` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `country_code` varchar(2) NOT NULL,
+  `country_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=491 DEFAULT CHARSET=utf8;
 
   --MUCHOS VIAJES TIENE UN PASAPORTE MANY TO ONE YYYYYYYYY muchos viajes TIENEn UN PAIS DE DESTINO
 
 DROP TABLE IF EXISTS `travels`;
 
 CREATE TABLE `travels` (
-  `idtravel` int(11) NOT NULL AUTO_INCREMENT,
+  `idTravel` int(11) NOT NULL AUTO_INCREMENT,
   `idCountry` int(11) NOT NULL,
   `startDate` date DEFAULT NULL,
   `endDate` date DEFAULT NULL,
   `purpose` varchar(45) DEFAULT NULL,
   `idPassport` int(11) NOT NULL,
-  PRIMARY KEY (`idtravel`),
+  PRIMARY KEY (`idTravel`),
   KEY `idCountry_idx` (`idCountry`),
   KEY `idPassport_idx` (`idPassport`),
   CONSTRAINT `idCountry` FOREIGN KEY (`idCountry`) REFERENCES `countries` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `idPassport` FOREIGN KEY (`idPassport`) REFERENCES `passport` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 
 
